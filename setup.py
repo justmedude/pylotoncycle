a127870dd46754fed69d135856ad51958bb1c598
Vikram Adukia <justmedude@users.noreply.github.com>
2020-06-09 09:35:20 -0700

    bump the version
    quick version bump, no changes of particular note for end users, though

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pylotoncycle',
-    version='0.1.3',
+    version='0.1.4',
     description='Module to access your peloton workout data',
     long_description=long_description,
     long_description_content_type="text/markdown",
