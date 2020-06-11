c4a9ddf6b219a5684e6517e5629886e136a2e803
Vikram Adukia <github@fireitup.net>
2020-06-11 05:01:12 +0000

    minor rev bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pylotoncycle',
-    version='0.2.0',
+    version='0.2.1',
     description='Module to access your Peloton workout data',
     long_description=long_description,
     long_description_content_type="text/markdown",
