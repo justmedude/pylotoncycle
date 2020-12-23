31c1cdcf043f06cd532e17ba828f4ed49fbe469e
Vikram Adukia <github@fireitup.net>
2020-12-23 13:24:03 -0800

    increment the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pylotoncycle',
-    version='0.2.3',
+    version='0.3.0',
     description='Module to access your Peloton workout data',
     long_description=long_description,
     long_description_content_type="text/markdown",
