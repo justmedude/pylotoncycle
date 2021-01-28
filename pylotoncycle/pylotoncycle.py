07830a8b065ba2395457bbca161a07bb444ce952
Toby Creek <toby@linuxwww.creek.home>
2021-01-28 09:55:28 -0500

    Forgot to remove the +1 in the loop

--- pylotoncycle/pylotoncycle.py
+++ pylotoncycle/pylotoncycle.py
@@ -82,7 +82,7 @@ class PylotonCycle:
 
         workout_list = []
         current_page = 0
-        for i in range(0, pages+1):
+        for i in range(0, pages):
             url = '%s&page=%s&limit=%s' % (
                 base_workout_url, current_page, limit)
             resp = self.s.get(url, timeout=10).json()
