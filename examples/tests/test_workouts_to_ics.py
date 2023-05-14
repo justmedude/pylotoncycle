8886a2d8d721675952c11dcefa4c038c5b4668bf
Craig Knudsen <craig@k5n.us>
2023-05-14 10:27:42 -0400

    Add unit tests for workout conversion to iCalendar format
    This commit adds unit tests to verify the correctness of the `convert_to_ical` function in the `workouts_to_ics.py` module. The new `TestConvertToICal` test class includes two test cases: one for a regular ride workout and another for a 'Just Ride' workout.
    
    The test cases ensure that the output of `convert_to_ical` matches the expected iCalendar data for the given workout data. The expected iCalendar data is generated based on the provided workout information, including the workout title, instructor name, duration, and other relevant details.
    
    The `generate_expected_ical_data` helper function is added to generate the expected iCalendar data based on the workout information.
    
    These changes enhance the test coverage and improve the reliability of the `convert_to_ical` function.
    
    Modified files:
    - examples/tests/test_workouts_to_ics.py

--- examples/tests/test_workouts_to_ics.py
+++ examples/tests/test_workouts_to_ics.py
@@ -1,5 +1,7 @@
 import unittest
-from workouts_to_ics import generate_description
+from datetime import datetime, timedelta
+from icalendar import Calendar, Event
+from workouts_to_ics import generate_description, convert_to_ical
 
 
 class TestGenerateDescription(unittest.TestCase):
@@ -67,6 +69,107 @@ Description: There’s nothing more satisfying than leaving a hill in your dust.
         description, error = generate_description(workout, True)
         self.assertEqual(description, expected_description)
         self.assertIsInstance(error, Exception)
+
+
+
+class TestConvertToICal(unittest.TestCase):
+
+    def test_convert_to_ical_regular_ride(self):
+        """
+        Test case to verify the conversion of a regular ride workout to iCalendar format.
+        """
+        workout = {
+            "created_at": 1672946636,
+            "ride": {
+                "title": "20 min Climb Ride",
+                "duration": 1273,
+                "instructor": {
+                    "name": "Emma Lovewell"
+                }
+            },
+            "instructor_name": "Emma Lovewell",
+            "fitness_discipline": "cycling",
+            "leaderboard_rank": 2375,
+            "total_leaderboard_users": 10267,
+            "ftp_info": {
+                "ftp": 213
+            }
+        }
+
+        expected_ical_data = self._generate_expected_ical_data(workout)
+
+        workouts = [workout]
+        ical_data = convert_to_ical(workouts)
+
+        self.assertEqual(ical_data, expected_ical_data)
+
+    def test_convert_to_ical_just_ride(self):
+        """
+        Test case to verify the conversion of a 'Just Ride' workout to iCalendar format.
+        """
+        workout = {
+            "created_at": 1672946636,
+            "ride": {
+                "title": "21 min Just Ride",
+                "duration": 1273,
+                "instructor": {
+                    "name": "JUST RIDE"
+                }
+            },
+            "instructor_name": "JUST RIDE",
+            "fitness_discipline": "cycling",
+            "ftp_info": {
+                "ftp": None,
+                "ftp_source": None,
+                "ftp_workout_id": None
+            }
+        }
+
+        expected_ical_data = self._generate_expected_ical_data(workout)
+
+        workouts = [workout]
+        ical_data = convert_to_ical(workouts)
+
+        self.assertEqual(ical_data, expected_ical_data)
+
+    def _generate_expected_ical_data(self, workout):
+        """
+        Generate the expected iCalendar data for a given workout.
+
+        Args:
+            workout (dict): The workout data.
+
+        Returns:
+            str: The expected iCalendar data.
+        """
+        cal = Calendar()
+        cal.add('VERSION', '2.0')
+        cal.add('PRODID', '-//pylotoncycle workouts_to_ics.py//EN')
+        cal.add('X-WR-CALNAME', 'Peloton Workouts')
+
+        created_at = datetime.fromtimestamp(int(workout['created_at']))
+        end_time = created_at + timedelta(minutes=workout['ride']['duration'] // 60)
+        workout_title = workout['ride'].get('title', 'Untitled')
+        instructor_name = workout['instructor_name']
+
+        if instructor_name == "JUST RIDE":
+            workout_len = workout['ride']['duration'] // 60
+            title = f"{workout_len} minute Just Ride"
+        else:
+            title = f"{workout_title} with {instructor_name}"
+
+        event = Event()
+        event.add('summary', title.encode('utf-8'))
+        event.add('dtstart', created_at)
+        event.add('dtend', end_time)
+
+        description, _ = generate_description(workout)
+        event.add('description', description.encode('utf-8'))
+
+        cal.add_component(event)
+        expected_ical_data = cal.to_ical().decode('utf-8')
+
+        return expected_ical_data
 
 
 if __name__ == '__main__':
