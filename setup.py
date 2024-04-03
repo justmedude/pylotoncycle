ca5d0e7c91de3b63bdf846a0c67453c9f967c5c2
adukia <adukia@stonewall.fireitup.net>
2024-04-02 23:00:50 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pylotoncycle",
-    version="0.7.0",
+    version="0.8.0",
     description="Module to access your Peloton workout data",
     long_description=long_description,
     long_description_content_type="text/markdown",
