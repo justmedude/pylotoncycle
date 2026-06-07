674cfaea4440077b8b4c2f6b7186ec3d5188c109
Vikram Adukia <vik.adukia@parentsquare.com>
2026-06-07 14:06:50 -0700

    Optimize and refactor ParseCyclingMetrics and ParseOutdoorRunMetrics parser functions

--- pylotoncycle/parser.py
+++ pylotoncycle/parser.py
@@ -1,79 +1,48 @@
 def ParseCyclingMetrics(json_resp):
-    duration = json_resp["duration"]
-
-    # Initialize a fast lookup dictionary to map seconds of the ride
-    # to a segment.
-    # The format of the dictionary is:
-    # seconds_since_pedaling_start : segment_name
-    # Initially, we know the duration of the ride, so we use that and init
-    # all segments as None
     segment_dict = {}
-    for i in range(0, duration + 1):
-        segment_dict[i] = None
-
-    # Now, for every segment that is listed, we want to map into segment_dict
-    # what the name of that segment is.
-    for i in json_resp["segment_list"]:
-        segment_name = i["name"]
-        start_time_offset = i["start_time_offset"]
-        end_time_offset = start_time_offset + i["length"]
-        # Using our start and end times, map into the segment_dict what
-        # segment name each second corresponds to
-        for j in range(start_time_offset, end_time_offset):
-            segment_dict[j] = segment_name
-
-    seconds_since_pedaling_start_list = json_resp[
-        "seconds_since_pedaling_start"
-    ]
-    counter = 0
+    for seg in json_resp.get("segment_list", []):
+        segment_name = seg["name"]
+        start = seg["start_time_offset"]
+        end = start + seg["length"]
+        for sec in range(start, end):
+            segment_dict[sec] = segment_name
 
     perf_dict = {}
-    for i in seconds_since_pedaling_start_list:
-        seconds_since_pedaling_start = seconds_since_pedaling_start_list[
-            counter
-        ]
+    metrics_cached = [
+        (m["slug"], m["values"]) for m in json_resp.get("metrics", [])
+    ]
+    seconds_since_pedaling_start_list = json_resp.get(
+        "seconds_since_pedaling_start", []
+    )
 
-        perf_dict[seconds_since_pedaling_start] = {}
-        for m in json_resp["metrics"]:
-            slug = m["slug"]
-            m_val = m["values"][counter]
-            perf_dict[seconds_since_pedaling_start][slug] = m_val
-            segment_name = segment_dict[seconds_since_pedaling_start]
-            perf_dict[seconds_since_pedaling_start]["segment"] = segment_name
-        counter += 1
+    for idx, seconds in enumerate(seconds_since_pedaling_start_list):
+        entry = {}
+        for slug, values in metrics_cached:
+            entry[slug] = values[idx]
+        entry["segment"] = segment_dict.get(seconds)
+        perf_dict[seconds] = entry
+
     return perf_dict
 
 
 def ParseOutdoorRunMetrics(json_resp):
-    # duration = json_resp['duration']
-
-    segment_list = json_resp["segment_list"]
-    segment_dict = {}
-    for i in segment_list:
-        segment_id = i["id"]
-        segment_name = i["name"]
-        segment_metrics_type = i["metrics_type"]
-        segment_dict[segment_id] = {
-            "segment_name": segment_name,
-            "segment_metrics_type": segment_metrics_type,
-        }
+    segment_dict = {
+        seg["id"]: (seg["name"], seg["metrics_type"])
+        for seg in json_resp.get("segment_list", [])
+    }
 
     perf_dict = {}
-    for i in json_resp["location_data"]:
-        segment_id = i["segment_id"]
-        segment_name = segment_dict[segment_id]["segment_name"]
-        segment_metrics_type = segment_dict[segment_id][
-            "segment_metrics_type"
-        ]
+    for location in json_resp.get("location_data", []):
+        segment_info = segment_dict.get(location["segment_id"])
+        if segment_info:
+            segment_name, segment_metrics_type = segment_info
+        else:
+            segment_name = segment_metrics_type = None
 
-        for datapoint in i["coordinates"]:
-            seconds_offset_from_start = datapoint["seconds_offset_from_start"]
-            perf_dict[seconds_offset_from_start] = datapoint
-            perf_dict[seconds_offset_from_start][
-                "segment_name"
-            ] = segment_name
-            perf_dict[seconds_offset_from_start][
-                "segment_metrics_type"
-            ] = segment_metrics_type
+        for coordinate in location.get("coordinates", []):
+            offset = coordinate["seconds_offset_from_start"]
+            coordinate["segment_name"] = segment_name
+            coordinate["segment_metrics_type"] = segment_metrics_type
+            perf_dict[offset] = coordinate
 
     return perf_dict
